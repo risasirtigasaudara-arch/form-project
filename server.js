8d8238afe04cedd01ed1ac62b091bd9dfc78410d
risasirtigasaudara-arch <risasir.tigasaudara@gmail.com>
2026-04-13 20:05:04 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit', (req, res) => {
+    const nama = req.body.nama;
+    const alamat = req.body.alamat;
+
+    console.log("=== DATA MASUK ===");
+    console.log("Nama:", nama);
+    console.log("Alamat:", alamat);
+
+    res.send("Data berhasil dikirim!");
+});
+
+app.listen(3000, () => {
+    console.log("Server jalan di http://localhost:3000");
+});
