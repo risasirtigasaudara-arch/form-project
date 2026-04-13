02babb6f0aa58389a1ecc6d75de8ba7eb502c1ed
risasirtigasaudara-arch <risasir.tigasaudara@gmail.com>
2026-04-13 20:18:57 +0700

    ready deploy

--- server.js
+++ server.js
@@ -23,3 +23,8 @@ app.post('/submit', (req, res) => {
 app.listen(3000, () => {
     console.log("Server jalan di http://localhost:3000");
 });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server jalan");
+});
